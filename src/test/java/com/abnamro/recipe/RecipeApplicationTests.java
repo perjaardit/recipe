ebb4983c1f6974963435157aea5586aadf9748a2
Ardit Perja <ardit.perja@lhind.dlh.de>
2022-11-27 18:46:25 +0100

    test class and properties changes

--- src/test/java/com/abnamro/recipe/RecipeApplicationTests.java
+++ src/test/java/com/abnamro/recipe/RecipeApplicationTests.java
@@ -3,11 +3,11 @@ package com.abnamro.recipe;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class RecipeApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+    @Test
+    void contextLoads() {
+    }
 
 }
